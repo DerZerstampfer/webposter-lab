133b6013ee4bf7d8b8b8ede8e1b970516f02fd90
Pau Kraft <beastmodepau@gmail.com>
2024-01-08 17:19:47 +0100

    🍱

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,6 @@ export default function Home() {
         <Image src={BGImage} alt="bg image" fill className="object-cover" />
       </div>
       <main className="flex min-h-[100svh] flex-col items-center justify-around px-4 gap-4">
-        {/* <Link href="/">
-          <div className="py-6 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-            <div className="text-6xl font-bold tracking-tight">Web Poster</div>
-          </div>
-        </Link> */}
-
         <div className="flex flex-col pt-6 md:gap-1 items-center">
           <Link href="/">
             <div className="font-mono text-4xl md:text-5xl">Webposter Lab</div>
@@ -30,6 +24,7 @@ export default function Home() {
         <div className="flex-1 flex items-center">
           <Generator />
         </div>
+
         <footer className="w-full flex flex-col justify-end py-6">
           <div className="container mx-auto flex items-center justify-between p-0">
             <Link
@@ -43,7 +38,7 @@ export default function Home() {
             <Link
               href=""
               className="text-xs text-gray-500 hover:underline flex items-end gap-1"
-              target={'_blank'}
+              target="_blank"
             >
               <Github className="h-4 w-4 text-white" />
               Source Code
