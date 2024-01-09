c841a22efeca2fd5620c335d359ec367881e3bb0
Pau Kraft <beastmodepau@gmail.com>
2024-01-09 11:56:20 +0100

    🔥

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-import NiceModal from '@ebay/nice-modal-react'
-
 type Props = {
   children?: React.ReactNode
 }
 
 export const Providers = ({ children }: Props) => {
-  return <NiceModal.Provider>{children}</NiceModal.Provider>
+  return <>{children}</>
 }
