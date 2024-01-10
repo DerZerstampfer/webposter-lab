18d6d636c61600cc739695a46098e180bcd75263
Pau Kraft <beastmodepau@gmail.com>
2024-01-10 11:06:03 +0100

    💬 github link

--- app/page.tsx
+++ app/page.tsx
@@ -8,24 +8,24 @@ import BGImage from '@/public/bg.png'
 export default function Home() {
   return (
     <>
-      <div className="fixed w-screen h-screen -z-10 opacity-10">
+      <div className="fixed -z-10 h-screen w-screen opacity-10">
         <Image src={BGImage} alt="bg image" fill className="object-cover" />
       </div>
-      <main className="flex min-h-[100svh] flex-col items-center justify-around px-4 gap-4">
-        <div className="flex flex-col pt-6 md:gap-1 items-center">
+      <main className="flex min-h-[100svh] flex-col items-center justify-around gap-4 px-4">
+        <div className="flex flex-col items-center pt-6 md:gap-1">
           <Link href="/">
             <div className="font-mono text-4xl md:text-5xl">Webposter Lab</div>
           </Link>
-          <span className="font-mono text-muted-foreground text-sm md:text-lg text-center">
+          <span className="text-center font-mono text-sm text-muted-foreground md:text-lg">
             Unique AI generated posters for any website
           </span>
         </div>
 
-        <div className="flex-1 flex items-center">
+        <div className="flex flex-1 items-center">
           <Generator />
         </div>
 
-        <footer className="w-full flex flex-col justify-end py-6">
+        <footer className="flex w-full flex-col justify-end py-6">
           <div className="container mx-auto flex items-center justify-between p-0">
             <Link
               href="https://twitter.com/paukraft"
@@ -36,8 +36,8 @@ export default function Home() {
             </Link>
 
             <Link
-              href=""
-              className="text-xs text-gray-500 hover:underline flex items-end gap-1"
+              href="https://github.com/DerZerstampfer/webposter-lab/"
+              className="flex items-end gap-1 text-xs text-gray-500 hover:underline"
               target="_blank"
             >
               <Github className="h-4 w-4 text-white" />
@@ -46,7 +46,7 @@ export default function Home() {
 
             <Link
               href="https://teampilot.ai/"
-              className="text-xs text-gray-500 hover:underline flex items-end gap-1"
+              className="flex items-end gap-1 text-xs text-gray-500 hover:underline"
               target={'_blank'}
             >
               <Image
