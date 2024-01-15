2dc42677452d763530e69e2431dc66f944f4a6c9
Pau Kraft <beastmodepau@gmail.com>
2024-01-15 15:17:13 +0100

    🩹

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Webposter Lab',
-  description: 'Unique AI generated posters for any website',
 }
 
 export default function RootLayout({
@@ -21,8 +20,16 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <head>
         <meta
+          name="description"
+          content="Unique AI generated posters tailored to any website with Webposter Lab"
+        />
+        <meta
           property="og:title"
           content="Webposter Lab - Unique AI generated posters for any website"
+        />
+        <meta
+          property="og:description"
+          content="Unique AI generated posters tailored to any website with Webposter Lab"
         />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://webposterlab.com" />
